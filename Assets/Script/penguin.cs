3770f02d7068d062d553613f8b19cbdf287231b2
aaaa18ec046 <40321113+aaaa18ec046@users.noreply.github.com>
2018-10-24 19:04:38 +0900

    add room2 to project
    room2追加
    いらないスクリプト消去

--- Assets/Script/penguin.cs
+++ Assets/Script/penguin.cs
@@ -3,22 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class penguin : MonoBehaviour {
-
-    [SerializeField,Tooltip("ペンギンが追従するオブジェクト")]
+    /// <summary>
+    /// 基本的にidou_flagで制御
+    /// idou_flagをずっとtrueにしてると二次関数的な移動を行う
+    /// 10/24現在のセッティングのままでは回転が終わったら移動となっている
+    /// </summary>
     private GameObject nekojarasi;
     private Vector3 neko_posi,nlz,posi,pengin_rotate,start_vector;
     private float x, z, neko_x, neko_z,direction_vector_x,direction_vector_z;
     [SerializeField]
-    private float speed = 0.1f,flap = 2f;
-    private Rigidbody rb;
-    private bool flag = true, isgrand = false,idouflag = false,jumpflag,abflag = true;
+    private float speed = 0.1f;
+    private bool flag = true, isgrand = true,idouflag = false;
     private IEnumerator Enumerable;
-    private int a = 1 ,b;
+    private int a = 1;
 
     void Awake()
     {
+        nekojarasi = GameObject.Find("nekojarashi");
         neko_posi = nekojarasi.transform.position; //ねこじゃらしの位置
-        b = GameObject.Find("nekojarashi1_set").GetComponent<nekojarasi>().flag; //ねこじゃらしが沢山ふられたかどうか
+        //b = GameObject.Find("nekojarashi1_set").GetComponent<nekojarasi>().flag; //ねこじゃらしが沢山ふられたかどうか
     }
 
 
@@ -28,24 +31,25 @@ public class penguin : MonoBehaviour {
         {
             moving();
         }
-        if (!isgrand && abflag)
+    /*    if (!isgrand && abflag)
         {
             abflag = false;
             StartCoroutine("Wait2");
-        }
+        }*/
     }
 
-    //
-    void moving()
+    //猫じゃらしがある方向への移動
+    public void moving()
     {
-        x = transform.position.x;
-        z = transform.position.z;
-        neko_x = neko_posi.x;
-        neko_z = neko_posi.z;
-        direction_vector_x = neko_x - x;
-        direction_vector_z = neko_z - z;
-        posi.Set(0, 0, -1);
+        x = transform.position.x; //無機物のx
+        z = transform.position.z; //無機物のz
+        neko_x = neko_posi.x;     //猫じゃらしのx
+        neko_z = neko_posi.z;     //猫じゃらしのz
+        direction_vector_x = x - neko_x; // 正の数…第２、３象限　負の数…第１、４象限
+        direction_vector_z = z - neko_z; // 正の数…第３、４象限　負の数…第１、２象限
 
+        posi.Set(0, 0, 1);
+        //移動する方向を向く(回転する)
         if(flag)
         {
             Enumerable = Rotation(direction_vector_x, direction_vector_z);
@@ -61,23 +65,17 @@ public class penguin : MonoBehaviour {
             {
                 Debug.Log("a");
                 idouflag = false;
-                jumpflag = true;
             }
         }
-        if(jumpflag)
-        {
-            Jump();
-            jumpflag = false; //falseにしないと無限ジャンプする
-        }
     }
-
+    //回転
+    //段階に分けて回転させたかったのでコルーチンで制御
     private IEnumerator Rotation(float dx,float dz)
     {
-        Debug.Log("a");
         dx *= -1;
         dz *= -1;
+        Debug.Log("a");
         float yziku = gameObject.transform.localEulerAngles.y * -1;
-        Debug.Log(yziku);
         float rad = Mathf.Atan2(dx, dz);
         float angle = rad * 180 / Mathf.PI;
         float angle_percentage = angle / 100;
@@ -91,13 +89,7 @@ public class penguin : MonoBehaviour {
         }
         idouflag = true;
     }
-    void Jump()
-    {
-        rb.AddForce(start_vector * 100f * flap);
-        start_vector = Vector3.up;
-        StartCoroutine("Wait1");
-    }
-    private void OnCollisionEnter(Collision collision)
+    /*private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Plane001" && a == 1)
         {
@@ -109,16 +101,9 @@ public class penguin : MonoBehaviour {
         {
             jumpflag = true;
         }
-    }
+    }*/
     IEnumerator Wait1()
     {
         yield return new WaitForSeconds(0.3f);
-    }
-    IEnumerator Wait2()
-    {
-        yield return new WaitForSeconds(5f);
-        start_vector = Vector3.up + Vector3.back;
-        rb = gameObject.GetComponent<Rigidbody>();
-        Jump();
     }
 }
