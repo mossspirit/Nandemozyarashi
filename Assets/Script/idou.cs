a58012d6fbe51a683bfdab9ae294fae891a8f791
aaaa18ec046 <40321113+aaaa18ec046@users.noreply.github.com>
2018-11-03 08:17:50 +0900

    add Animator
    Chair Scene での追加
    移動してるかしてないかidouスクリプトをflagで判定した
    どのような場合であってもアニメーションするようにした

--- Assets/Script/idou.cs
+++ Assets/Script/idou.cs
@@ -9,20 +9,25 @@ public class idou : MonoBehaviour {
     [SerializeField]
     private float hanni;
     private bool flag = false;
+    [System.NonSerialized]
+    public bool a_flag = true;
     [SerializeField]
     private GameObject obj; 
     private nekojarasi nekojarashi;
     private float x, z, neko_x, neko_z, direction_vector_x, direction_vector_z;
     private Vector3 neko_posi;
+    private Animator animator;
+    private string[] anim_name = { "walk","taiki", "zyare1", "zyare2", "jump1" };
 
     void Start ()
     {
         nekojarashi = obj.GetComponent<nekojarasi>();
         neko_posi = GameObject.Find("[CameraRig]").transform.position;
+        animator = GetComponent<Animator>();
 	}
 
 
-    void Update()
+    void FixedUpdate()
     {
         x = transform.position.x; //無機物のx       
         z = transform.position.z; //無機物のz       
@@ -33,19 +38,39 @@ public class idou : MonoBehaviour {
 
         flag = nekojarashi.idou_flag;
 
-        if (Mathf.Abs(Mathf.Sqrt(Mathf.Pow((direction_vector_x), 2) + Mathf.Pow((direction_vector_z), 2))) >= hanni)
-        {
-            if (flag)
-            {
-                Debug.Log(Mathf.Abs(Mathf.Sqrt(Mathf.Pow((direction_vector_x), 2) + Mathf.Pow((direction_vector_z), 2))) + " " + direction_vector_x + " " + direction_vector_z);
-
-                transform.position += transform.forward * speed;
-            }
-        }
+        StartCoroutine(Moving(direction_vector_x, direction_vector_z));
         /*if(flag && !((direction_vector_x <= hanni) && (direction_vector_x >= -hanni) && (direction_vector_z <= hanni) && (direction_vector_z >= -hanni)))
         {
             Debug.Log(gameObject.name + " " + direction_vector_x + " " + direction_vector_z);
             transform.position += transform.forward * speed;
         }*/
+    }
+
+    private IEnumerator Moving(float dx,float dz)
+    {
+        flag = nekojarashi.idou_flag;
+
+        if (Mathf.Abs(Mathf.Sqrt(Mathf.Pow((direction_vector_x), 2) + Mathf.Pow((direction_vector_z), 2))) >= hanni)
+        {
+            if (flag)
+            {
+                //Debug.Log(Mathf.Abs(Mathf.Sqrt(Mathf.Pow((direction_vector_x), 2) + Mathf.Pow((direction_vector_z), 2))) + " " + direction_vector_x + " " + direction_vector_z);
+                transform.position += transform.forward * speed;
+                animator.SetBool(anim_name[0],true);
+            }
+            else
+            {
+                animator.SetBool(anim_name[0], false);
+            }
+        }
+        else if (Mathf.Abs(Mathf.Sqrt(Mathf.Pow((direction_vector_x), 2) + Mathf.Pow((direction_vector_z), 2))) < hanni)
+        {
+            if (flag)
+            {
+                var i = Random.Range(1, 3);
+                animator.SetTrigger(anim_name[i]);
+            }
+        }
+        yield return new WaitForSeconds(0.5f);
     }
 }
--- Assets/Script/nekojarasi.cs
+++ Assets/Script/nekojarasi.cs
@@ -11,7 +11,7 @@ public class nekojarasi : MonoBehaviour
     /// 
     private float new_data = 0, old_data = 0, c = 0;
     private int i;
-    private bool rotation_flag;
+    private bool rotation_flag,animation_flag;
     [SerializeField]
     private float sa_hensu = 100f;
 
@@ -20,15 +20,16 @@ public class nekojarasi : MonoBehaviour
     [System.NonSerialized]
     public bool idou_flag = false;
     private int flag;
+    [SerializeField]
+    private GameObject doubutsu;
 
-
-    public void Start()
+     void Start()
     {
 
     }
     void FixedUpdate()
     {
-
+        animation_flag = doubutsu.GetComponent<idou>().a_flag;
         c++;
         if (c >= 2)
         {
@@ -42,10 +43,15 @@ public class nekojarasi : MonoBehaviour
 
             if (Hantei(new_data, old_data) && flag >= 1)
             {
+                Debug.Log("a");
                 idou_flag = true;
+
             }
             else
+            {
+                Debug.Log("b");
                 idou_flag = false;
+            }
         }
         new_data = old_data;
     }
